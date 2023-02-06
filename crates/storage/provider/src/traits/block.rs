1b1e536227541535b70a1683aefd44233ae35e5b
Georgios Konstantopoulos <me@gakonst.com>
2023-02-05 21:52:47 -0800

    refactor

--- crates/storage/provider/src/traits/block.rs
+++ crates/storage/provider/src/traits/block.rs
@@ -1,7 +1,11 @@
 use std::ops::Range;
 
 use super::BlockHashProvider;
-use reth_db::{cursor::DbCursorRO, tables, transaction::DbTx};
+use reth_db::{
+    cursor::DbCursorRO,
+    tables,
+    transaction::{DbTx, DbTxMut},
+};
 use reth_interfaces::Result;
 use reth_primitives::{
     rpc::{BlockId, BlockNumber},
@@ -64,20 +68,62 @@ pub trait BlockProvider: BlockHashProvider + Send + Sync {
 }
 
 /// Utilities for querying larger ranges of blocks
-pub trait SealedBlockProvider {
+pub trait DbTxExt {
     /// Given a range, it proceeds to return a Vec<SealedBlock> for that range.
     /// Will query all of the: Headers, Bodies, Senders,
-    fn sealed_block_range(&self, range: Range<usize>) -> Result<Vec<(SealedBlock, Vec<Address>)>>;
+    fn sealed_block_range(
+        &self,
+        range: Range<usize>,
+    ) -> Result<Vec<(SealedBlock, u64, Vec<Address>)>>;
+}
+
+pub trait DbTxMutExt {
+    /// Given a bunch of blocks it'll proceed to write them all to the database, creating
+    /// all the necessary
+    fn write_blocks(&self, blocks: Vec<(SealedBlock, u64, Vec<Address>)>) -> Result<()>;
 }
 
 use reth_db::models::BlockNumHash;
 use reth_interfaces::provider::Error as ProviderError;
 use reth_primitives::Address;
 
-#[async_trait::async_trait]
-impl<'a, Tx: DbTx<'a>> SealedBlockProvider for Tx {
-    fn sealed_block_range(&self, range: Range<usize>) -> Result<Vec<(SealedBlock, Vec<Address>)>> {
-        let tx = self;
+pub struct SealedBlocksProvider<'a, Tx>(&'a Tx);
+
+impl<'a, Tx> SealedBlocksProvider<'a, Tx> {
+    pub fn write_blocks(&self, blocks: Vec<(SealedBlock, u64, Vec<Address>)>) -> Result<()>
+    where
+        Tx: DbTxMut<'a>,
+    {
+        let tx = self.0;
+
+        // Get next canonical block hashes to execute.
+        let mut canonicals = tx.cursor_write::<tables::CanonicalHeaders>()?;
+        // Get header with canonical hashes.
+        let mut headers = tx.cursor_write::<tables::Headers>()?;
+        // Get bodies with canonical hashes.
+        let mut bodies_cursor = tx.cursor_write::<tables::BlockBodies>()?;
+        // Get ommers with canonical hashes.
+        let mut ommers_cursor = tx.cursor_write::<tables::BlockOmmers>()?;
+        // Get transaction of the block that we are executing.
+        let mut tx_cursor = tx.cursor_write::<tables::Transactions>()?;
+        // Skip sender recovery and load signer from database.
+        let mut tx_sender = tx.cursor_write::<tables::TxSenders>()?;
+
+        for (block, start_tx_id, senders) in blocks {
+            let mut tx_sender_walker = tx_sender.walk(start_tx_id)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn sealed_block_range(
+        &self,
+        range: Range<usize>,
+    ) -> Result<Vec<(SealedBlock, u64, Vec<Address>)>>
+    where
+        Tx: DbTx<'a>,
+    {
+        let tx = self.0;
         let start_block = range.start as u64;
         let end_block = range.end as u64;
 
@@ -94,85 +140,80 @@ impl<'a, Tx: DbTx<'a>> SealedBlockProvider for Tx {
         // Skip sender recovery and load signer from database.
         let mut tx_sender = tx.cursor_read::<tables::TxSenders>()?;
 
-        // get canonical blocks (num,hash)
-        let canonical_batch = canonicals
-            .walk_range(start_block..end_block + 1)?
-            .map(|i| i.map(BlockNumHash))
-            .collect::<std::result::Result<Vec<_>, _>>()?;
+        let blocks =
+            canonicals
+                .walk_range(start_block..end_block + 1)?
+                .map(|i| i.map(BlockNumHash))
+                .map(|key| {
+                    let key = key?;
 
-        // Get block headers and bodies from canonical hashes
-        let block_batch = canonical_batch
-            .iter()
-            .map(|key| {
-                // NOTE: It probably will be faster to fetch all items from one table with cursor,
-                // but to reduce complexity we are using `seek_exact` to skip some
-                // edge cases that can happen.
-                let (_, header) = headers
-                    .seek_exact(*key)?
-                    .ok_or(ProviderError::Header { number: key.number(), hash: key.hash() })?;
-                let (_, body) =
-                    bodies_cursor.seek_exact(*key)?.ok_or(ProviderError::BlockBody {
-                        block_number: key.number(),
-                        block_hash: key.hash(),
-                    })?;
-                let (_, stored_ommers) = ommers_cursor.seek_exact(*key)?.unwrap_or_default();
+                    // NOTE: It probably will be faster to fetch all items from one table with
+                    // cursor, but to reduce complexity we are using
+                    // `seek_exact` to skip some edge cases that can happen.
+                    let (_, header) = headers
+                        .seek_exact(key)?
+                        .ok_or(ProviderError::Header { number: key.number(), hash: key.hash() })?;
+                    let (_, body) =
+                        bodies_cursor.seek_exact(key)?.ok_or(ProviderError::BlockBody {
+                            block_number: key.number(),
+                            block_hash: key.hash(),
+                        })?;
+                    let (_, stored_ommers) = ommers_cursor.seek_exact(key)?.unwrap_or_default();
+                    let ommers = stored_ommers.ommers;
 
-                Ok((header, body, stored_ommers.ommers))
-            })
-            .collect::<Result<Vec<_>>>()?;
+                    let block_number = header.number;
+                    tracing::trace!(?block_number, "getting transactions and senders");
+                    // iterate over all transactions
+                    let mut tx_walker = tx_cursor.walk(body.start_tx_id)?;
+                    let mut transactions = Vec::with_capacity(body.tx_count as usize);
+                    // get next N transactions.
+                    for index in body.tx_id_range() {
+                        let (tx_index, tx) =
+                            tx_walker.next().ok_or(ProviderError::EndOfTransactionTable)??;
+                        if tx_index != index {
+                            tracing::error!(
+                                block = block_number,
+                                expected = index,
+                                found = tx_index,
+                                ?body,
+                                "Transaction gap"
+                            );
+                            return Err(ProviderError::TransactionsGap { missing: tx_index }.into())
+                        }
+                        transactions.push(tx);
+                    }
 
-        let mut blocks = Vec::with_capacity(block_batch.len());
+                    // take signers
+                    let mut tx_sender_walker = tx_sender.walk(body.start_tx_id)?;
+                    let mut signers = Vec::with_capacity(body.tx_count as usize);
+                    for index in body.tx_id_range() {
+                        let (tx_index, tx) = tx_sender_walker
+                            .next()
+                            .ok_or(ProviderError::EndOfTransactionSenderTable)??;
+                        if tx_index != index {
+                            tracing::error!(
+                                block = block_number,
+                                expected = index,
+                                found = tx_index,
+                                ?body,
+                                "Signer gap"
+                            );
+                            return Err(
+                                ProviderError::TransactionsSignerGap { missing: tx_index }.into()
+                            )
+                        }
+                        signers.push(tx);
+                    }
 
-        // // Fetch transactions, execute them and generate results
-        for (header, body, ommers) in block_batch.into_iter() {
-            let block_number = header.number;
-            tracing::trace!(?block_number, "getting transactions and senders");
-            // iterate over all transactions
-            let mut tx_walker = tx_cursor.walk(body.start_tx_id)?;
-            let mut transactions = Vec::with_capacity(body.tx_count as usize);
-            // get next N transactions.
-            for index in body.tx_id_range() {
-                let (tx_index, tx) =
-                    tx_walker.next().ok_or(ProviderError::EndOfTransactionTable)??;
-                if tx_index != index {
-                    tracing::error!(
-                        block = block_number,
-                        expected = index,
-                        found = tx_index,
-                        ?body,
-                        "Transaction gap"
-                    );
-                    return Err(ProviderError::TransactionsGap { missing: tx_index }.into())
-                }
-                transactions.push(tx);
-            }
+                    let block = SealedBlock {
+                        header: header.seal(),
+                        ommers: ommers.iter().cloned().map(|x| x.seal()).collect(),
+                        body: transactions,
+                    };
 
-            // take signers
-            let mut tx_sender_walker = tx_sender.walk(body.start_tx_id)?;
-            let mut signers = Vec::with_capacity(body.tx_count as usize);
-            for index in body.tx_id_range() {
-                let (tx_index, tx) =
-                    tx_sender_walker.next().ok_or(ProviderError::EndOfTransactionSenderTable)??;
-                if tx_index != index {
-                    tracing::error!(
-                        block = block_number,
-                        expected = index,
-                        found = tx_index,
-                        ?body,
-                        "Signer gap"
-                    );
-                    return Err(ProviderError::TransactionsSignerGap { missing: tx_index }.into())
-                }
-                signers.push(tx);
-            }
-
-            let block = SealedBlock {
-                header: header.clone().seal(),
-                ommers: ommers.iter().cloned().map(|x| x.seal()).collect(),
-                body: transactions,
-            };
-            blocks.push((block, signers));
-        }
+                    Ok((block, body.start_tx_id, signers))
+                })
+                .collect::<Result<Vec<_>>>()?;
 
         Ok(blocks)
     }
